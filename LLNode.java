4262e2d718b9f0ac53bcc3b005ee32dfb2989432
bhawani06 <bhawaniselva@gmail.com>
2017-05-20 11:46:18 -0700

    Add files via upload

---
+++ LLNode.java
@@ -0,0 +1,25 @@
+package linkList;
+
+public class LLNode {
+
+	int data;
+	LLNode next;
+
+	public void setData(int val)
+	{
+		data = val;
+	}
+	public int getData()
+	{
+		return data;
+	}
+	public void setNext(LLNode n)
+	{
+		next =n;
+	}
+	public LLNode getNode()
+	{
+		return next;
+	}
+	
+}
